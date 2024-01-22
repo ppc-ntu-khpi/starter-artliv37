5dc25d4d9158f688bb1507ad8d1ba9b64f07d37d
artliv37 <137114966+artliv37@users.noreply.github.com>
2024-01-22 02:19:34 +0200

    Delete src/src directory

--- src/src/Quotation.java
+++
@@ -1,9 +0,0 @@
-public class Quotation {
-    String quote = "“The only true wisdom is knowing that you know nothing.”";
-    String author = "Socrates";
-
-    public void display() {
-        System.out.println(quote);
-        System.out.println("- " + author);
-    }
-}
